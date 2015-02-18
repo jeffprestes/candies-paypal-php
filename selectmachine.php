c99398edd39288c83625735c1bcb8dc8103e8368
Jeff Prestes <jeffprestes@gmail.com>
2015-02-18 12:18:21 -0200

    Add link to Mobile App at Play Store

--- selectmachine.php
+++ selectmachine.php
@@ -49,7 +49,7 @@ if ($app == 'checkin')      {
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script type="text/javascript">    
             function tradicional(){
-                jQuery('<div class="sa_payPal_overlay" style="visibility:visible;position:fixed; width:100%; height:100%; filter:progid:DXImageTransform.Microsoft.Gradient(GradientType=1, StartColorStr=\'#88ffffff\', EndColorStr=\'#88ffffff\'); background: rgba(255,255,255,0.8); top:0; left:0; z-index: 999999;"><div style=" background: #FFF; background-image: linear-gradient(top, #FFFFFF 45%, #E9ECEF 80%);background-image: -o-linear-gradient(top, #FFFFFF 45%, #E9ECEF 80%);background-image: -moz-linear-gradient(top, #FFFFFF 45%, #E9ECEF 80%);background-image: -webkit-linear-gradient(top, #FFFFFF 45%, #E9ECEF 80%);background-image: -ms-linear-gradient(top, #FFFFFF 45%, #E9ECEF 80%);background-image: -webkit-gradient(linear, left top,left bottom,color-stop(0.45, #FFFFFF),color-stop(0.8, #E9ECEF));display: block;margin: auto;position: fixed; margin-left:-120px; left:45%;top: 40%;text-align: center;color: #2F6395;font-family: Arial;padding: 15px;font-size: 15px;font-weight: bold;width: 330px;-webkit-box-shadow: 3px 2px 13px rgba(50, 50, 49, 0.25);box-shadow: rgba(0, 0, 0, 0.2) 0px 0px 0px 5px;border: 1px solid #CFCFCF;border-radius: 6px;"><img style="display:block;margin:0 auto 10px" src="https://www.paypalobjects.com/en_US/i/icon/icon_animated_prog_dkgy_42wx42h.gif"><h2>Aguarde segundos...</h2> <div style="margin:30px auto 0;"><img src="https://www.paypal-brasil.com.br/logocenter/util/img/logo_paypal.png"/></div></div></div>').appendTo('body');
+                jQuery('<div class="sa_payPal_overlay" style="visibility:visible;position:fixed; width:100%; height:100%; filter:progid:DXImageTransform.Microsoft.Gradient(GradientType=1, StartColorStr=\'#88ffffff\', EndColorStr=\'#88ffffff\'); background: rgba(255,255,255,0.8); top:0; left:0; z-index: 999999;"><div style=" background: #FFF; background-image: linear-gradient(top, #FFFFFF 45%, #E9ECEF 80%);background-image: -o-linear-gradient(top, #FFFFFF 45%, #E9ECEF 80%);background-image: -moz-linear-gradient(top, #FFFFFF 45%, #E9ECEF 80%);background-image: -webkit-linear-gradient(top, #FFFFFF 45%, #E9ECEF 80%);background-image: -ms-linear-gradient(top, #FFFFFF 45%, #E9ECEF 80%);background-image: -webkit-gradient(linear, left top,left bottom,color-stop(0.45, #FFFFFF),color-stop(0.8, #E9ECEF));display: block;margin: auto;position: fixed; margin-left:-120px; left:45%;top: 40%;text-align: center;color: #2F6395;font-family: Arial;padding: 15px;font-size: 15px;font-weight: bold;width: 330px;-webkit-box-shadow: 3px 2px 13px rgba(50, 50, 49, 0.25);box-shadow: rgba(0, 0, 0, 0.2) 0px 0px 0px 5px;border: 1px solid #CFCFCF;border-radius: 6px;"><img style="display:block;margin:0 auto 10px" src="https://www.paypalobjects.com/en_US/i/icon/icon_animated_prog_dkgy_42wx42h.gif"><h2>Please wait...</h2> <div style="margin:30px auto 0;"><img src="https://www.paypal-brasil.com.br/logocenter/util/img/logo_paypal.png"/></div></div></div>').appendTo('body');
                 document.frmPurchase.submit();
             }
         </script>
@@ -59,15 +59,21 @@ if ($app == 'checkin')      {
             <img src="images/gumball-machine.gif" id="imgcandy" />
             <br />
             <br />
-            <span id="message">Escolha a maquina</span>
+            <span id="message">Choose your machine: </span>
             <form id="frmPurchase" name="frmPurchase" action="redirect.php" method="POST">
                 <select id="location" name="location">
-                    <option value="jeffprestes/candies/world" selected="selected">Brazil, Sao Paulo, Campus Party</option>
-                    <!-- <option value="jeffprestes/candies/demo" selected="selected">Brazil, Sao Paulo, Demonstration</option> -->
+                    <option value="jeffprestes/candies/paulista" selected="selected">Brazil, Sao Paulo, Alameda Santos</option>
+                    <option value="jeffprestes/candies/world">Brazil, Sao Paulo, Demonstration</option>
                 </select>
                 <br /><br />
                 <input type="button" id="btn" value="" onclick="tradicional()" />
             </form>
+        </div>
+        <br /><br />
+        <div id="mobileapp">
+            <a href="http://play.google.com/store/apps/details?id=br.com.novatrix.candies">
+                <img border="0" src="http://r-ec.bstatic.com/static/img/mobile/apps_landing/bapps-ps/7d831795522cf482dd76604ae6a41e59164c463d.png" />
+            </a>
         </div>
     </body>
 </html>
